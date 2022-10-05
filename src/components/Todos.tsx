2d4ff7a2951d72c4443c4fae256fd71ac55ca0db
LewisCM14 <Lewiscmartin@live.co.uk>
2022-10-05 18:20:22 +0100

    define the todo.ts model and wire up to Todos component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
+import Todo from "../models/todo";
+
 // uses the type FunctionalComponent, ensuring the children prop is always viable
 // then defines the generic type field of this type
-// as an object with a key of items that expects an array of strings
-const Todos: React.FC<{ items: string[] }> = (props) => {
+// as an object with a key of items that expects an array with the structure defined in the todo.ts model
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
