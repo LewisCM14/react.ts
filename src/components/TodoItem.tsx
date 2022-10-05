59506a9968ad0409709a1e42cf6fff98b143d725
LewisCM14 <Lewiscmartin@live.co.uk>
2022-10-05 18:29:37 +0100

    handle structuring TodoItems outside of Todos.tsx

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,9 @@
+// constructs the structure of each individual todo item, passed to Todos.tsx
+
+const TodoItem: React.FC<{text: string}> = (props) => {
+    return (
+        <li>{props.text}</li>
+    )
+}
+
+export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import TodoItem from "./TodoItem";
 import Todo from "../models/todo";
 
 // uses the type FunctionalComponent, ensuring the children prop is always viable
@@ -9,7 +10,7 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text}/>
       ))}
     </ul>
   );
