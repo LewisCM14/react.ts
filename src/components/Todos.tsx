31c0c8a3b12e7bff88c99698086a3782f74e7506
LewisCM14 <Lewiscmartin@live.co.uk>
2022-10-05 18:03:53 +0100

    define type in Todos component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,16 @@
-function Todos() {
-    return (
-        <ul>
-            <li>Learn React</li>
-            <li>Learn TypeScript</li>
-        </ul>
-    );
+import React from "react";
+
+// uses the type FunctionalComponent, ensuring the children prop is always viable
+// then defines the generic type field of this type
+// as an object with a key of items that expects an array of strings
+const Todos: React.FC<{ items: string[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default Todos;
